f630e0246cfcb905edd880a9d23dbca237cd7ad8
Hung Nguyen <hungnm144@live.com>
2019-03-07 02:06:58 +0700

    Force return Json Exceptions for API via Browsers

--- Exceptions/Handlers/ExceptionsHandler.php
+++ Exceptions/Handlers/ExceptionsHandler.php
@@ -1,5 +1,5 @@
 <?php
-echo 123;
+
 namespace Apiato\Core\Exceptions\Handlers;
 
 use Exception;
@@ -16,7 +16,7 @@ use Illuminate\Foundation\Exceptions\Handler as LaravelExceptionHandler;
 class ExceptionsHandler extends HeimdalExceptionHandler
 {
     public function render($request, Exception $e)
-    {var_dump($request->expectsJson());die;
+    {
         // if the user expects json or the API forces the user to send it
         if (($request->expectsJson()) || (Config::get('apiato.requests.force-accept-header') && $e instanceof MissingJSONHeaderException) || $_SERVER['HTTP_HOST'] == (parse_url(Config::get('apiato.api.url'))['host'])) {
             // return the error as json
